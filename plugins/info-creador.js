c31ca77b86ae56befa6e2b309e8b32cb8e98cc34
vhhvf <122591347+vhhvf@users.noreply.github.com>
2023-01-21 15:28:48 +0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -3,16 +3,16 @@ function handler(m, { conn }) {
 let vn = './media/creador.mp3'
 let text = `
 *𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙤* 
-*Wa.me/5492266466080 (no bot)*
-*Wa.me/447520621851 (bot)*
-*Wa.me/524421134524 (bot²)*
+*Wa.me/967730348571 (no bot)*
+*Wa.me/967771160204 (bot)*
+*Wa.me/967771160204 (bot²)*
 `.trim()   
 conn.reply(m.chat, text, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '𝑻𝒉𝒆 𝑳𝒐𝒍𝒊𝑩𝒐𝒕-𝑴𝑫',
+title: '𝑻𝒉𝒆انوس-𝑴𝑫',
 body: '𝐂𝐫𝐞𝐚𝐝𝐨𝐫',         
 previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
-sourceUrl: `https://wa.me/5492266466080`}}})
+sourceUrl: `https://wa.me/967730348571`}}})
   
 //const data = global.owner.filter(([id, isCreator]) => id && isCreator)
 //this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
@@ -20,7 +20,7 @@ sourceUrl: `https://wa.me/5492266466080`}}})
 let pp = './media/menus/Menu2.jpg'
 let str = `🧡 *Eso son los contactos para ti.*`
 
-conn.sendHydrated(m.chat, str, wm, pp, 'https://chat.whatsapp.com/Lus9S60MABnH9lF4Wf2T7k', 'grupos', null, null, [
+conn.sendHydrated(m.chat, str, wm, pp, 'https://chat.whatsapp.com/LO17u6F9Fw4LE9izmmyOGz', 'grupos', null, null, [
 ['🐈 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣', '.infobot'],
 ['🎁 𝘿𝙤𝙣𝙖𝙧', '.donar'],
 ['☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤', '/menu']
